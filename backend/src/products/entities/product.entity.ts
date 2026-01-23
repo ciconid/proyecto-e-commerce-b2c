a1c0885d8a430c3c312fad443ca7aba75a1b3777
cico <nicocidips@gmail.com>
2026-01-22 21:42:05 -0300

    Hacer downgrade a prisma@5.
    Dejar funcionando operaciones CRUD basicas con una BD Postgres dockerizada

---
+++ backend/src/products/entities/product.entity.ts
@@ -0,0 +1,10 @@
+export class Product {
+    id: string;
+    name: string;
+    description?: string;
+    price: number; 
+    stock: number; 
+    imageUrl?: string;
+    createdAt: Date;
+    updatedAt: Date;    
+}
