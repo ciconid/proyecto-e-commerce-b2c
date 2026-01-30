13682732416c7c2dd7a62a9197dffa9cf0bfca68
cico <nicocidips@gmail.com>
2026-01-30 04:25:39 -0300

    feat: configurar TanStack Query para manejo de datos del servidor

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,33 @@
-import { StrictMode } from 'react'
-import './index.css'
-import App from './App.tsx'
-import '@mantine/core/styles.css'
-import '@mantine/notifications/styles.css'
-import ReactDOM from 'react-dom/client'
-import { MantineProvider } from '@mantine/core'
-import { Notifications } from '@mantine/notifications'
+import React from 'react';
+import './index.css';
+import App from './App.tsx';
+import '@mantine/core/styles.css';
+import '@mantine/notifications/styles.css';
+import ReactDOM from 'react-dom/client';
+import { MantineProvider } from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+            retry: 1,
+            staleTime: 5 * 60 * 1000        // Datos "frescos" por 5 minutos
+        }
+    }
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <MantineProvider>
-            <Notifications position="top-right" />
-            <App />
-        </MantineProvider>
-    </StrictMode>,
-)
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <MantineProvider >
+                <Notifications position="top-right" />
+                <App />
+                <ReactQueryDevtools initialIsOpen={false} />
+            </MantineProvider>
+        </QueryClientProvider>
+    </React.StrictMode>,
+);
