7dbffb78086434fdace211b83278cb8bd24daf97
cico <nicocidips@gmail.com>
2026-01-30 04:15:43 -0300

    feat: configurar Mantine para UI

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import '@mantine/core/styles.css'
+import '@mantine/notifications/styles.css'
+import ReactDOM from 'react-dom/client'
+import { MantineProvider } from '@mantine/core'
+import { Notifications } from '@mantine/notifications'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <MantineProvider>
+            <Notifications position="top-right" />
+            <App />
+        </MantineProvider>
+    </StrictMode>,
 )
--- frontend/src/pages/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage.tsx
@@ -1,17 +1,19 @@
+import { Button, Container, Text, Title } from '@mantine/core';
 import { useNavigate } from 'react-router-dom';
 
 function NotFoundPage() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  return (
-    <div style={{ textAlign: 'center', marginTop: '100px' }}>
-      <h1>404</h1>
-      <p>Página no encontrada</p>
-      <button onClick={() => navigate('/products')}>
-        Volver al inicio
-      </button>
-    </div>
-  );
+    return (
+        <Container style={{ textAlign: 'center', marginTop: '100px' }}>
+            <Title order={1}>404</Title>
+            <Text size="lg" mt="md">Página no encontrada</Text>
+            <Button mt="xl" onClick={() => navigate('/products')}>
+                Volver al inicio
+            </Button>
+        </Container>
+
+    );
 }
 
 export default NotFoundPage;
