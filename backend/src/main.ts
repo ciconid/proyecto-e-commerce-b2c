3c190a919fc71748362fd57edbbb63190d2a8627
ciconid <nicocidips@gmail.com>
2026-02-08 07:01:02 -0300

    fix: problemas con la API route

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,8 @@ import { ZodValidationPipe } from 'nestjs-zod';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
+    app.setGlobalPrefix("api");
+
     // Habilitar CORS
     app.enableCors({
         origin: "http://localhost:5173",
@@ -28,6 +30,7 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api/docs', app, document);
 
+    
 
     await app.listen(process.env.PORT ?? 3000);
 }
