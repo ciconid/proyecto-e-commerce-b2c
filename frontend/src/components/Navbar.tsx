005e48159ed2b9a09be9eeaf85a202c15c729c23
ciconid <nicocidips@gmail.com>
2026-02-12 01:08:50 -0300

    style: mostrar nombre de usuario en la UI. Hacerlo menu desplegable y poner Cerrar Sesion ahi adentro
    centrar contenidos de navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,6 +1,6 @@
-import { ActionIcon, Anchor, Badge, Box, Group, Text } from "@mantine/core";
+import { ActionIcon, Anchor, Badge, Box, Container, Group, Menu, Text } from "@mantine/core";
 import { Link, useNavigate } from "react-router-dom";
-import { IconLogout } from "@tabler/icons-react"
+import { IconChevronDown, IconLogout, IconUser } from "@tabler/icons-react"
 import { useAuthStore } from "../store/authStore";
 import { useCartStore } from "../store/cartStore";
 
@@ -28,49 +28,71 @@ function Navbar() {
                 zIndex: 100
             }}
         >
-            <Group justify="space-between">
-                <Text size="xl" fw={700} c={"blue"}>Mi tienda</Text>
+            <Container size={"xl"}>
+                <Group justify="space-between">
+                    <Text size="xl" fw={700} c={"blue"}>Mi tienda</Text>
 
-                <Group>
-                    <Anchor component={Link} to={"/products"}>
-                        Productos
-                    </Anchor>
-
-                    <Anchor component={Link} to={"/cart"} style={{ position: "relative" }}>
-                        Carrito
-                        {totalItems > 0 &&
-                            <Badge
-                                size="md"
-                                circle
-                                color="red"
-                                style={{
-                                    position: "absolute",
-                                    top: -8,
-                                    right: -12
-                                }}
-                            >
-                                {totalItems}
-                            </Badge>
-                        }
-                    </Anchor>
-
-                    <Anchor component={Link} to={"/orders"}>
-                        Mis Órdenes
-                    </Anchor>
-
-                    {user?.role === "admin" &&
-                        <Anchor component={Link} to={"/admin"}>
-                            Admin Panel
+                    <Group>
+                        <Anchor component={Link} to={"/products"}>
+                            Productos
                         </Anchor>
-                    }
 
+                        <Anchor component={Link} to={"/cart"} style={{ position: "relative" }}>
+                            Carrito
+                            {totalItems > 0 &&
+                                <Badge
+                                    size="md"
+                                    circle
+                                    color="red"
+                                    style={{
+                                        position: "absolute",
+                                        top: -8,
+                                        right: -12
+                                    }}
+                                >
+                                    {totalItems}
+                                </Badge>
+                            }
+                        </Anchor>
 
-                    <ActionIcon onClick={handleLogout}>
-                        <IconLogout />
-                    </ActionIcon>
+                        <Anchor component={Link} to={"/orders"}>
+                            Mis Órdenes
+                        </Anchor>
+
+                        {user?.role === "admin" &&
+                            <Anchor component={Link} to={"/admin"}>
+                                Admin Panel
+                            </Anchor>
+                        }
+
+                        <Menu shadow="md" width={200}>
+                            <Menu.Target>
+                                <Group gap={4} style={{ cursor: "pointer" }}>
+                                    <IconUser size={18} />
+                                    <Text size="sm" fw={500}>
+                                        {user?.name}
+                                    </Text>
+                                    <IconChevronDown size={14} />
+                                </Group>
+                            </Menu.Target>
+
+                            <Menu.Dropdown>
+                                <Menu.Label>{user?.email}</Menu.Label>
+                                <Menu.Divider />
+                                <Menu.Item
+                                    color="red"
+                                    leftSection={<IconLogout size={16} />}
+                                    onClick={handleLogout}
+                                >
+                                    Cerrar sesión
+                                </Menu.Item>
+                            </Menu.Dropdown>
+                        </Menu>
+                    </Group>
+
                 </Group>
+            </Container>
 
-            </Group>
         </Box>
     );
 }
